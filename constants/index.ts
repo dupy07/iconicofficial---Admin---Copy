2d956feff71c57af07ff018132e0862c1c66ac1b
dupy07 <piyush.xtha5@gmail.com>
2024-06-08 01:23:30 +0545

    Description of changes

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,19 @@
+export const NavLinks = [
+  {
+    href: "/Featured",
+    key: "New & Featured",
+    text: "New & Featured",
+  },
+  {
+    href: "/Men",
+    key: "Men",
+    text: "Men",
+  },
+  { href: "/Women", key: "Women", text: "Women" },
+  { href: "/Sale", key: "Blog", text: "Sale" },
+  { href: "/Contacts", key: "Contacts", text: "Contacts" },
+];
+
 export const SideBarLinks = [
   {
     href: "/Dashboard",
@@ -17,7 +33,7 @@ export const SideBarLinks = [
   {
     href: "/Products",
     key: "Products",
-    text: "Products",
+    text: "Prdoucts",
   },
   {
     href: "/Orders",
