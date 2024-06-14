b2eb1b4b4d83a31f0dc66b85233e80ec2f5220ba
dupy07 <piyush.xtha5@gmail.com>
2024-06-15 01:14:01 +0545

    Description of changes

--- components/Product.tsx
+++ components/Product.tsx
@@ -348,7 +348,7 @@ const ProductComponent: React.FC = () => {
         />
       </div>
 
-      <div className="rounded-md border">
+      <div className="overflow-x-auto rounded-md border">
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
