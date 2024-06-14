12e6c5d21bb32840fc7dda6ff309ae15d1823372
dupy07 <piyush.xtha5@gmail.com>
2024-06-15 00:55:59 +0545

    Description of changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,8 @@
 import React from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { ModeToggle } from "@/components/ui/toggle-mode";
+import { ModeToggle } from "./ui/toggle-mode";
+import Image from "next/image";
 
 const Header: React.FC<{ toggleSidebar: () => void }> = ({ toggleSidebar }) => {
   return (
@@ -11,11 +12,17 @@ const Header: React.FC<{ toggleSidebar: () => void }> = ({ toggleSidebar }) => {
         <span className="text-sm">Welcome to Iconic Official Admin Page</span>
       </div>
       <div className="rounded-tr-md rounded-br-md border border-gray-300 bg-background flex justify-between items-center h-20 p-5 text-gray-700">
-        <div className="flex items-center gap-2 md:gap-4">
+        <div className="flex items-center  gap-2 md:gap-3">
           <div className="md:hidden cursor-pointer" onClick={toggleSidebar}>
             <CiMenuBurger size={24} />
           </div>
-          <img src="/profile-1.png" alt="Logo" className="w-12 h-12" />
+          <Image
+            src="/profile-1.png"
+            alt="Logo"
+            width={40}
+            height={40}
+            className="w-10 h-10 sm:w-12 sm:h-12"
+          />
           <h2 className="text-xl md:text-2xl font-bold">
             ICO<span className="text-red-500">NIC</span>
           </h2>
