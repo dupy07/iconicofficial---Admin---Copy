cde520a380393f3463f8d83bce269f0f3005c865
dupy07 <piyush.xtha5@gmail.com>
2024-06-24 18:51:56 +0545

    Description of changes

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -70,7 +70,7 @@ const BarChart: React.FC = () => {
   }, []);
 
   return (
-    <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-background ">
+    <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-secondary ">
       <Bar data={chartData} options={chartOptions} />
     </div>
   );
