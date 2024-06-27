cf6e8cd694fe0deba01a3aa95b32ad78b3a21bab
dupy07 <piyush.xtha5@gmail.com>
2024-06-27 21:00:17 +0545

    Description of changes

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,10 +1,16 @@
-"use client"; // pages/dashboard.tsx
-import { useEffect, useState } from "react";
+// pages/dashboard.tsx
+"use client";
+import { useEffect, useState, useMemo } from "react";
 import BarChart from "@/components/BarChart";
 import Layout from "@/components/Layout";
 import RecentOrder from "@/components/RecentOrder";
 import Image from "next/image";
 
+interface StatsCardProps {
+  title: string;
+  value: string | number;
+  imageSrc: string;
+}
 interface Order {
   _id: string;
   totalAmount: number;
@@ -45,66 +51,16 @@ const Dashboard = () => {
   const [categories, setCategories] = useState<Category[]>([]);
 
   useEffect(() => {
-    const fetchProductsAndCategories = async () => {
+    const fetchData = async () => {
       try {
-        const [resCategories, resProducts, resOrders] = await Promise.all([
-          fetch("/api/categories"),
-          fetch("/api/products"),
-          fetch("/api/orders"),
-        ]);
-
-        if (!resProducts.ok || !resCategories.ok || !resOrders.ok) {
-          throw new Error("Failed to fetch data");
-        }
-
         const [categoriesResult, productsResult, ordersResult] =
           await Promise.all([
-            resCategories.json(),
-            resProducts.json(),
-            resOrders.json(),
+            fetchDataFromApi("/api/categories"),
+            fetchDataFromApi("/api/products"),
+            fetchDataFromApi("/api/orders"),
           ]);
 
-        if (
-          productsResult.success &&
-          categoriesResult.success &&
-          ordersResult.success
-        ) {
-          setProducts(productsResult.data);
-          setCategories(categoriesResult.data);
-
-          const orders: Order[] = ordersResult.data;
-
-          // Filter orders to include only those that are paid and delivered
-          const paidAndDeliveredOrders = orders.filter(
-            (order) =>
-              order.orderStatus === "Delivered" &&
-              order.paymentStatus === "Paid"
-          );
-
-          // Calculate total revenue
-          const revenue = paidAndDeliveredOrders.reduce(
-            (acc, order) => acc + order.totalAmount,
-            0
-          );
-          setTotalRevenue(revenue);
-
-          // Calculate orders count
-          setOrdersCount(orders.length);
-
-          // Calculate available stock
-          const stock = productsResult.data.reduce(
-            (acc: number, product: Product) =>
-              acc +
-              product.variants.reduce(
-                (variantAcc, variant) => variantAcc + variant.quantity,
-                0
-              ),
-            0
-          );
-          setAvailableStock(stock);
-        } else {
-          throw new Error("Failed to fetch products or categories");
-        }
+        handleFetchResults(categoriesResult, productsResult, ordersResult);
       } catch (error: any) {
         setError(error.message);
       } finally {
@@ -112,19 +68,68 @@ const Dashboard = () => {
       }
     };
 
-    fetchProductsAndCategories();
+    fetchData();
   }, []);
 
-  const getGreeting = () => {
-    const currentHour = new Date().getHours();
-    if (currentHour < 12) {
-      return "Good morning";
+  const fetchDataFromApi = async (endpoint: string) => {
+    const response = await fetch(endpoint);
+    if (!response.ok) throw new Error(`Failed to fetch ${endpoint}`);
+    return response.json();
+  };
+
+  const handleFetchResults = (
+    categoriesResult: any,
+    productsResult: any,
+    ordersResult: any
+  ) => {
+    if (
+      productsResult.success &&
+      categoriesResult.success &&
+      ordersResult.success
+    ) {
+      setProducts(productsResult.data);
+      setCategories(categoriesResult.data);
+
+      const orders: Order[] = ordersResult.data;
+
+      // Filter orders to include only those that are paid and delivered
+      const paidAndDeliveredOrders = orders.filter(
+        (order) =>
+          order.orderStatus === "Delivered" && order.paymentStatus === "Paid"
+      );
+
+      // Calculate total revenue
+      const revenue = paidAndDeliveredOrders.reduce(
+        (acc, order) => acc + order.totalAmount,
+        0
+      );
+      setTotalRevenue(revenue);
+
+      // Calculate orders count
+      setOrdersCount(orders.length);
+
+      // Calculate available stock
+      const stock = productsResult.data.reduce(
+        (acc: number, product: Product) =>
+          acc +
+          product.variants.reduce(
+            (variantAcc, variant) => variantAcc + variant.quantity,
+            0
+          ),
+        0
+      );
+      setAvailableStock(stock);
     } else {
-      return "Good afternoon";
+      throw new Error("Failed to fetch products or categories");
     }
   };
 
-  const getCurrentDate = () => {
+  const getGreeting = useMemo(() => {
+    const currentHour = new Date().getHours();
+    return currentHour < 12 ? "Good morning" : "Good afternoon";
+  }, []);
+
+  const getCurrentDate = useMemo(() => {
     const today = new Date();
     return today.toLocaleDateString(undefined, {
       weekday: "long",
@@ -132,7 +137,7 @@ const Dashboard = () => {
       month: "long",
       day: "numeric",
     });
-  };
+  }, []);
 
   return (
     <Layout>
@@ -143,59 +148,30 @@ const Dashboard = () => {
       ) : (
         <>
           <h1 className="font-semibold text-black dark:text-primary text-xl tracking-tight">
-            {getGreeting()}
+            {getGreeting}
           </h1>
           <h1 className="pb-4 fs-300 tracking-tight text-black dark:text-primary">
-            {" "}
             Here are your stats for today,{" "}
             <span className="text-[#8830f7] dark:text-[#ad85ff] tracking-tight">
-              {getCurrentDate()}
+              {getCurrentDate}
             </span>
           </h1>
           <div className="pb-4 grid lg:grid-cols-3 gap-4">
-            <div className="bg-secondary border flex flex-col md:flex-row justify-between w-full p-4 rounded-lg">
-              <div className="flex flex-col w-full pb-4">
-                <Image
-                  src={"/revenue.png"}
-                  alt="revenue"
-                  width={40}
-                  height={40}
-                  className="pb-4"
-                />
-                <p className="text-[#adb5bd] pb-2"> Revenue</p>
-                <p className="text-3xl font-bold text-primary">
-                  रू {totalRevenue.toFixed(2)}
-                </p>
-              </div>
-            </div>
-            <div className="bg-secondary border flex flex-col md:flex-row justify-between w-full p-4 rounded-lg">
-              <div className="flex flex-col w-full pb-4">
-                <Image
-                  src={"/order.png"}
-                  alt="revenue"
-                  width={40}
-                  height={40}
-                  className="pb-4"
-                />
-                <p className="text-[#adb5bd] pb-2">Orders</p>
-                <p className="text-3xl font-bold text-primary">{ordersCount}</p>
-              </div>
-            </div>
-            <div className="bg-secondary border flex flex-col md:flex-row justify-between w-full p-4 rounded-lg">
-              <div className="flex flex-col w-full pb-4">
-                <Image
-                  src={"/stock.png"}
-                  alt="revenue"
-                  width={40}
-                  height={40}
-                  className="pb-4"
-                />
-                <p className="text-[#adb5bd] pb-2"> Stock</p>
-                <p className="text-3xl font-bold text-primary">
-                  {availableStock}
-                </p>
-              </div>
-            </div>
+            <StatsCard
+              title="Revenue"
+              value={`रू ${totalRevenue.toFixed(2)}`}
+              imageSrc="/revenue.png"
+            />
+            <StatsCard
+              title="Orders"
+              value={ordersCount}
+              imageSrc="/order.png"
+            />
+            <StatsCard
+              title="Stock"
+              value={availableStock}
+              imageSrc="/stock.png"
+            />
           </div>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-4">
             <BarChart />
@@ -206,5 +182,21 @@ const Dashboard = () => {
     </Layout>
   );
 };
+
+const StatsCard: React.FC<StatsCardProps> = ({ title, value, imageSrc }) => (
+  <div className="bg-secondary border flex flex-col md:flex-row justify-between w-full p-4 rounded-lg">
+    <div className="flex flex-col w-full pb-4">
+      <Image
+        src={imageSrc}
+        alt={title}
+        width={40}
+        height={40}
+        className="pb-4"
+      />
+      <p className="text-[#adb5bd] pb-2">{title}</p>
+      <p className="text-3xl font-bold text-primary">{value}</p>
+    </div>
+  </div>
+);
 
 export default Dashboard;
